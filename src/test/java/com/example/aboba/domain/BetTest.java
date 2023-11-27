3ecd5d713d17087788283e4846dbac7701cac188
Jegor Popow <juicedogegor@gmail.com>
2023-11-27 19:39:47 +0300

    bet test

---
+++ src/test/java/com/example/aboba/domain/BetTest.java
@@ -0,0 +1,30 @@
+package com.example.aboba.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RouletteTest {
+    @Test
+    void singleBet() {
+        Bet bet1 = new Bet("SINGLE", 15, 1000);
+        Bet bet2 = new Bet("SINGLE", 30, 1000);
+        Assertions.assertThrows(Exception.class, () -> new Bet("SINGLE", 100, 0));
+        Assertions.assertEquals(bet1.GetWinMoney(15), 1000 * 36);
+        Assertions.assertEquals(bet2.GetWinMoney(15), 1000 * 0);
+    }
+
+    @Test
+    void colorBet() {
+        Bet bet1 = new Bet("RED", 0, 1000);
+        Bet bet2 = new Bet("BLACK", 0, 1000);
+        Bet bet3 = new Bet("GREEN", 0, 1000);       
+        Assertions.assertEquals(bet1.GetWinMoney(15), 1000 * 0);
+        Assertions.assertEquals(bet2.GetWinMoney(15), 1000 * 2);
+        Assertions.assertEquals(bet3.GetWinMoney(0), 1000 * 36);
+
+    }
+
+
+}
